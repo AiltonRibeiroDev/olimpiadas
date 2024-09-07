8ead038e9f023e3b5c22f57ba1bee9244b97822e
Ailton Alves Ribeiro <91134990+AiltonRibeiroDev@users.noreply.github.com>
2024-09-07 18:35:01 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao:
+            "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao:
+            "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Sua técnica e determinação a tornaram um ícone do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" ,
+        tags: "judo judoca "
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao:
+            "Rebeca Andrade é uma ginasta brasileira que se destacou nos Jogos Olímpicos de Tóquio 2020, conquistando a medalha de prata no individual geral. Sua versatilidade e carisma a tornaram uma das maiores ginastas brasileiras de todos os tempos.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" ,
+        tags: "ginástica ginastica ouro"
+    }
+];
